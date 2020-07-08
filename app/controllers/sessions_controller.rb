c9c7d1b56a291953395e2b54d3be9b3c2a4d94b2
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-08 21:54:56 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if session[:name] = params[:name] 
+    if session[:name] = params[:name]
       redirect_to '/'
-    elsif
+    elsif session[:name] == nil && session[:name] == []
       redirect_to 'sessions#new'
     end
   end
